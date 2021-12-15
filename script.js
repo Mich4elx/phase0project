502d6a54dd678b3b97cd5fa74fbb93225a11ba1d
Michael Martinez <michaelm47x@gmail.com>
2021-12-14 20:00:46 -0600

    finished

---
+++ script.js
@@ -0,0 +1,11 @@
+const headerImg = document.getElementById('headerImg');
+
+headerImg.addEventListener('click',function(){
+    alert("I don't do anything");
+})
+
+const link = document.getElementsByClassName('contactLink')
+
+link.addEventListener('click', function(){
+    alert("I don't do anything");
+})
